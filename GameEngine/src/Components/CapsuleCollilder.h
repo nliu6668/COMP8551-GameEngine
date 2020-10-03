7f5c3c66737add5e1820d5908af7387ffc9e47e3
Billy Wong <ocis_nicola@hotmail.com>
2020-10-03 11:02:34 -0700

    Added physics components

---
+++ GameEngine/src/Components/CapsuleCollilder.h
@@ -0,0 +1,18 @@
+#pragma once
+
+struct 2D_CapsuleCollider {
+	2D_CapsuleCollider(float radius = 0.0f, float a = 0.0f,
+	float x = 0.0f, float y = 0.0f, float z = 0.0f,
+	float bbX1 = 0.0f, float bbY1 = 0.0f, float bbX2 = 0.0f, float bbY2 = 0.0f,
+	float bbX3 = 0.0f, float bbY3 = 0.0f, float bbX4 = 0.0f, float bbY4 = 0.0f,
+	bool isTrigger = false, bool render = false) :
+	radius(radius), x(x), y(y), z(z), bbX1(bbX1), bbY1(bbY1),
+	bbX2(bbX2), bbY2(bbY2), bbX3(bbX3), bbY3(bbY3), bbX4(bbX4), bbY4(bbY4),
+	isTrigger(isTrigger), render(render){}
+	
+	float radius, a;     //dimensions  (a represents the length between the ends of the capsule)
+	float x, y, z;    //position (relative to entity)
+	float bbX1, bbY1, bbX2, bbY2, bbX3, bbY3, bbX4, bbY4;  //bounding box for AABB
+	bool isTrigger;   //specifies if collider is a trigger
+	bool render;      //specifies whether to render the collider
+}
--- GameEngine/src/Components/Components.h
+++ GameEngine/src/Components/Components.h
@@ -3,3 +3,7 @@
 
 //include all component structs
 #include "Transform.h"
+#include "2D_Rigidbody.h"
+#include "BoxCollider.h"
+#include "CircleCollider.h"
+#include "CapsuleCollider.h"
