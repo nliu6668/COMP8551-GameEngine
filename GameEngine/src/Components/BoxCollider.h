ee36cb7563e17d8c0d473de19f5d77012a75b2bc
Ian Younghusband <ianyounghusband@hotmail.com>
2020-10-03 18:13:10 -0700

    fixed boxcollider bug

--- GameEngine/src/Components/BoxCollider.h
+++ GameEngine/src/Components/BoxCollider.h
@@ -3,7 +3,6 @@
 struct BoxCollider {
 	BoxCollider(float width = 0.0f, float height = 0.0f, 
 	float x = 0.0f, float y = 0.0f, float z = 0.0f,
-	float bbHeight, float bbWidth,
 	bool isTrigger = false, bool render = false) :
 	width(width), height(height), x(x), y(y), z(z), bbHeight(height),
 	bbWidth(width), isTrigger(isTrigger), render(render), b(x - bbWidth / 2.0f), e(x + bbWidth / 2.0f){}
