2d1b280c6b825705fb7bc11253a52ec615f6042e
Billy Wong <ocis_nicola@hotmail.com>
2020-10-03 18:04:29 -0700

    Changed boxcollider

--- GameEngine/src/Components/BoxCollider.h
+++ GameEngine/src/Components/BoxCollider.h
@@ -3,10 +3,10 @@
 struct BoxCollider {
 	BoxCollider(float width = 0.0f, float height = 0.0f, 
 	float x = 0.0f, float y = 0.0f, float z = 0.0f,
-	float bbHeight = 0.0f, float bbWidth = 0.0f,
+	float bbHeight, float bbWidth,
 	bool isTrigger = false, bool render = false) :
-	width(width), height(height), x(x), y(y), z(z), bbHeight(bbHeight),
-	bbWidth(bbWidth), isTrigger(isTrigger), render(render){}
+	width(width), height(height), x(x), y(y), z(z), bbHeight(height),
+	bbWidth(width), isTrigger(isTrigger), render(render){}
 	
 	float width, height;  //dimensions
 	float x, y, z;        //position (relative to the entity)
