1d0a5c8a1c8ceaa0c5e9b7eb742d302204fd6eb8
Darian Gratton <dariangratton@gmail.com>
2020-10-06 00:10:57 -0700

    Implementation of dynamically compiling a string of code

--- GameEngine/src/Systems/CustomScriptSystem.h
+++ GameEngine/src/Systems/CustomScriptSystem.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <windows.h>
+
 #include "entityx/entityx.h"
 #include "../Components/CustomScript.h"
 
@@ -10,8 +15,83 @@ public:
     {
         es.each<CustomScript>([dt](Entity entity, CustomScript &customScript) {
 
-            customScript.cScript->update();
+            //customScript.cScript->update();
         
         });
-    }      
-};
+
+        toFile(ccodestring);
+
+        CScript* cscript;
+        cscript = getCustomScriptObject("CustomTestScript");
+        
+        if (cscript != NULL)
+            cscript->update();
+        else
+            std::cout << "getCustomScriptObject failed" << std::endl;
+    }
+
+    void toFile(std::string ccode)
+    {
+        ofstream myfile;
+        myfile.open("src/CustomScripts/CustomTestScript.cpp");
+        myfile << ccode;
+        myfile.close();
+    }
+
+    CScript* getCustomScriptObject(std::string filename)
+    {
+        HINSTANCE hdll = NULL;
+        CScript* cscript = NULL;
+        typedef void* (*pvScriptObject)();
+        pvScriptObject createCustomScriptObject;
+
+        // Create dll file 
+        std::string gccCommand = "g++ src/CustomScripts/" + filename + ".cpp -o src/CustomScripts/" + filename + ".dll -shared -fPIC";
+        const char *cgccCommand = gccCommand.c_str();
+        system(cgccCommand);
+        
+        // Load the dll ( It says there is an error but it compiles and works anyways :/ )
+        std::string path = "CustomScripts\\CustomTestScript.dll";
+        const char * cpath = path.c_str();
+        hdll = LoadLibrary("src\\CustomScripts\\CustomTestScript.dll");
+
+        if (!hdll)
+        {
+           std::cout << "Failed to load dll " << filename << std::endl;
+           return NULL;
+        }
+
+        // Get the function pointer
+        createCustomScriptObject = (pvScriptObject) (GetProcAddress(hdll, "CreateCustomScriptObject"));
+
+        if (!createCustomScriptObject)
+        {
+            std::cout << "Failed to find CreateCustomScriptObject in " << filename << std::endl;
+            return NULL;
+        }
+
+        // Create CScript Object
+        cscript = static_cast<CScript*> ( createCustomScriptObject() ); 
+        
+        return cscript;
+    }
+
+private:
+    std::string ccodestring = 
+        "#include <iostream> \n"  
+        "#include \"../CScript.h\" \n\n"
+        "#define EXPORT extern \"C\" __declspec(dllexport) \n\n"  
+        "class CustomTestScript : public CScript \n" 
+        "{ \n" 
+        "public: \n" 
+        "   void update(); \n" 
+        "}; \n\n" 
+        "void CustomTestScript::update() \n" 
+        "{ \n" 
+        "   std::cout << \"Test Script Update Function\" << std::endl; \n" 
+        "} \n\n"
+        "EXPORT CScript* CreateCustomScriptObject() \n"
+        "{ \n"
+        "   return new CustomTestScript(); \n"
+        "} \n";
+}; 
