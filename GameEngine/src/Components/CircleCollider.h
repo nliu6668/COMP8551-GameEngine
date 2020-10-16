52e326d6281c8ad773f7215d193051d86f174bfe
Ian Younghusband <ianyounghusband@hotmail.com>
2020-10-15 20:22:05 -0700

    refactored collider code to work with all collider types so far

--- GameEngine/src/Components/CircleCollider.h
+++ GameEngine/src/Components/CircleCollider.h
@@ -1,14 +1,15 @@
 #pragma once
 
 struct CircleCollider {
-	CircleCollider(float radius = 0.0f, float x = 0.0f, float y = 0.0f, float z = 0.0f,
+	CircleCollider(float radius = 1.0f, float x = 0.0f, float y = 0.0f, float z = 0.0f,
 	bool isTrigger = false, bool render = false) :
 	radius(radius), x(x), y(y), z(z), bbHeight(radius * 2),
-	bbWidth(radius * 2), isTrigger(isTrigger), render(render){}
+	bbWidth(radius * 2), isTrigger(isTrigger), render(render), b(x - bbWidth / 2.0f), e(x + bbWidth / 2.0f) {}
 	
 	float radius;     //dimensions
 	float x, y, z;    //position (relative to entity)
 	float bbHeight, bbWidth;  //bounding box for AABB
+	float b, e;
 	bool isTrigger;   //specifies if collider is a trigger
 	bool render;      //specifies whether to render the collider
 };
